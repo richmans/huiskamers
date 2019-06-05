75814e38b941cd5ee726b5101e3d8563d00f3449
Joris <jorisvoermans@live.nl>
2019-06-05 19:48:34 +0200

    Changed version number to correct value

--- huiskamers.php
+++ huiskamers.php
@@ -3,7 +3,7 @@
 Plugin Name: Huiskamers
 Plugin URI: http://github.com/richmans/huiskamers
 Description: Provides a plugin for huiskamers.nl to administer a list of local groups. It allows visitors to connect to the groups by sending an email.
-Version: 1.10
+Version: 1.11
 Author: Richard Bronkhorst & Joris Voermans
 License: GPL2
 */
